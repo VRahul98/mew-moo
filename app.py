bdbb0bd4c46184f268997763ff4a4aefe861cd14
VRahul98 <Vasanthapuramrahul@gmail.com>
2025-07-31 01:40:26 +0530

    Fix indentation error in app.py

--- app.py
+++ app.py
@@ -41,9 +41,8 @@ def update_product():
 
     return redirect(url_for('admin'))
 
-if __name__ == '__main__':
 import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host="0.0.0.0", port=port)
