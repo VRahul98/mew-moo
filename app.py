2df1f947b30ebefe6d5cb5fa8cc6aa84fafa342a
VRahul98 <Vasanthapuramrahul@gmail.com>
2025-08-02 03:58:07 +0530

    🔧 Fixed internal server error and updated admin panel

--- app.py
+++ app.py
@@ -217,3 +217,6 @@ def delete_product():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
